d3ee1664f7159ca7333c6e483489d8ea66ca6b92
Magnus Karström <83014443+mrkarstrom@users.noreply.github.com>
2021-10-23 14:01:42 +0200

    Created the board and first eventlisteners in app.js

---
+++ Pacman/app.js
@@ -0,0 +1,33 @@
+const board = ['pink', 'blue', 'green', 'red', 'purple', 'orange'];
+const myBoard = [];
+const tempBoard = [
+    1,1,1,1,1,1,1,1,1,1,
+    1,2,3,2,2,2,2,2,2,1,
+    1,2,1,1,1,1,1,1,2,1,
+    1,2,2,2,2,2,2,2,2,1,
+    1,2,1,1,1,1,1,1,2,1,
+    1,2,2,2,2,2,2,2,2,1,
+    1,2,1,1,1,1,1,1,2,1,
+    1,2,2,2,2,2,2,2,2,1,
+    1,2,2,2,2,2,2,2,2,1,
+    1,1,1,1,1,1,1,1,1,1
+];
+
+const ghost = [];
+
+const g = {
+    x: '', y: '', h: 100, size: 10, ghosts: 6, inplay: false
+}
+
+const player = {
+    pos: 20, speed: 4, cool: 0, pause: false
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    g.grid = document.querySelector('.grid');  //gameBoard
+    g.pacman = document.querySelector('.pacman'); //pacman
+    g.eye = document.querySelector('.eye');
+    g.mouth = document.querySelector('.mouth');
+    g.ghost = document.querySelector('.ghost');
+    g.ghost.style.display = 'none';
+})
